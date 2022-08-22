2c658c7a0895f9b3cafd271adaedbd8f8023b964
calvintzeng96 <calvintzeng96@hotmail.com>
2022-08-22 00:13:03 -0400

    leetcode #26 updated

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -20,18 +20,18 @@
 // If all assertions pass, then your solution will be accepted.
 
 var removeDuplicates = function(nums) {
-    let answer = [...nums];
-    let index = 0;
-    let tracked = new Set()
-    for (let i = 0; i < nums.length; i++) {
-        if (!tracked.has(nums[i])) {
-            tracked.add(nums[i]);
-            answer[index] = nums[i]
-            index++
-        }
+let index = 0;
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i-1]) {
+        nums[index] = nums[i]
+        index++
     }
-    return answer
+}
+return nums
 };
+//leetcode wants to return index...not nums
+
+
 
 let input1 = [1,1,2]
 let input2 = [0,0,1,1,1,2,2,3,3,4]
