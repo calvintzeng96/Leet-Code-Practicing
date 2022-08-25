61ed433b63ea63f46c19d1dc66a2c8b8016cb7b4
calvintzeng96 <calvintzeng96@hotmail.com>
2022-08-25 17:41:57 -0400

    merge two sorted list passed

--- merge-two-sorted-list.js
+++ merge-two-sorted-list.js
@@ -2,18 +2,39 @@
 // You are given the heads of two sorted linked lists list1 and list2.
 // Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 // Return the head of the merged linked list.
-
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
 
 var mergeTwoLists = function(list1, list2) {
 
+    let curr = new ListNode()
+    let holder = curr
+    while(list1 && list2) {
+        if (list1.val < list2.val) {
+            curr.next = list1
+            list1 = list1.next;
+        } else {
+            curr.next = list2
+            list2 = list2.next;
+        }
+        curr = curr.next
+    }
+    if (list1) {
+        curr.next = list1
+    } else {
+        curr.next = list2
+    }
+    return holder.next
 };
 
-let list1 = [1,2,4]
-let list2 =[1,3,4]
-console.log(mergeTwoLists(list1, list2)) //[1,1,2,3,4,4]
-let list3 = []
-let list4 = []
-console.log(mergeTwoLists(list3, list4)) //[]
+// let list1 = [1,2,4]
+// let list2 =[1,3,4]
+// console.log(mergeTwoLists(list1, list2)) //[1,1,2,3,4,4]
+// let list3 = []
+// let list4 = []
+// console.log(mergeTwoLists(list3, list4)) //[]
 
 
 // Example 1:
