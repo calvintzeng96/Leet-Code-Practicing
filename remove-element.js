32e75f8f94b8fc4280f74a373c5b5ed388717022
calvintzeng96 <calvintzeng96@hotmail.com>
2022-09-05 22:07:56 -0400

    remove element mastered

--- remove-element.js
+++ remove-element.js
@@ -20,7 +20,14 @@
 // If all assertions pass, then your solution will be accepted.
 
 var removeElement = function(nums, val) {
-
+    let count = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[count] = nums[i]
+            count++
+        }
+    }
+    return count
 };
 
 
